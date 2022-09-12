ebc982a5f8cab24a62e658f80d02c43614a9d342
kukkusruthi <nsruthi054@gmail.com>
2022-09-12 21:44:56 +0530

    created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VegComponent } from './veg/veg.component';
+import { ChocolatesComponent } from './chocolates/chocolates.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VegComponent,
+    ChocolatesComponent
   ],
   imports: [
     BrowserModule,
